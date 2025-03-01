8a37e8e7d8e7eace6f18433ce3c10d539c2a85ff
MIFTAH UL HAQ <miftah4165@gmail.com>
2025-03-01 19:26:59 +0600

    Added more Typed JS

--- js/script.js
+++ js/script.js
@@ -33,23 +33,58 @@ var containerEl = document.querySelector('.mixitUP');
 /* Typed JS End */
 $(function(){
 	$(".typed").typed({
-		strings: ["Developers.", "Designers.", "People."],
+		strings: ["Lightbox is small javascript library used to overlay images on top of the current page. It's a snap to setup and works on all modern browsers."],
 		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 		stringsElement: null,
 		// typing speed
-		typeSpeed: 30,
+		typeSpeed: 0,
 		// time before typing starts
-		startDelay: 1200,
+		startDelay: 1000,
 		// backspacing speed
 		backSpeed: 20,
 		// time before backspacing
-		backDelay: 500,
+		backDelay: 7000,
 		// loop
 		loop: true,
 		// false = infinite
 		loopCount: 5,
 		// show cursor
-		showCursor: false,
+		showCursor: true,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either html or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
+$(function(){
+	$(".typed").typed({
+		strings: ["Lightbox is small javascript library used to overlay images on top of the current page. It's a snap to setup and works on all modern browsers."],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 0,
+		// time before typing starts
+		startDelay: 1000,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 7000,
+		// loop
+		loop: true,
+		// false = infinite
+		loopCount: 5,
+		// show cursor
+		showCursor: true,
 		// character for cursor
 		cursorChar: "|",
 		// attribute to type (null == text)
